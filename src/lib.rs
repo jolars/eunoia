03340a1ee29a3ef47b9c9549db8e38181f56128e
Johan Larsson <johan@jolars.co>
2025-11-21 13:22:39 +0100

    chore: remove unused trait

--- src/lib.rs
+++ src/lib.rs
@@ -30,26 +30,3 @@ pub mod geometry;
 // Re-export main API types
 pub use diagram::{Combination, Diagram, DiagramBuilder, InputType};
 pub use error::DiagramError;
-
-/// Trait for shapes that can be parameterized for optimization.
-///
-/// This trait enables shapes to expose their degrees of freedom (position, size, rotation)
-/// as a parameter vector for use with optimization algorithms.
-pub trait Parameters {
-    /// Returns the number of parameters needed to fully describe this shape.
-    ///
-    /// For example, a circle has 3 parameters (x, y, radius), while an ellipse
-    /// has 5 (x, y, semi-major axis, semi-minor axis, rotation angle).
-    fn n_params(&self) -> usize;
-
-    /// Updates the shape's parameters from a parameter vector.
-    ///
-    /// # Arguments
-    ///
-    /// * `params` - A slice containing the new parameter values
-    ///
-    /// # Panics
-    ///
-    /// May panic if the length of `params` doesn't match `n_params()`.
-    fn update(&mut self, params: &[f64]);
-}
