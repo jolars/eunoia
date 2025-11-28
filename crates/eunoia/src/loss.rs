3370be0a0d6f4ca4f555641a0722b38b9b9ef3b9
Johan Larsson <johan@jolars.co>
2025-11-28 14:13:47 +0100

    test: add tests for loss module

--- crates/eunoia/src/loss.rs
+++ crates/eunoia/src/loss.rs
@@ -197,3 +197,324 @@ impl LossFunction for ConfigurableLoss {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_aggregation_default() {
+        assert_eq!(Aggregation::default(), Aggregation::SumAbsolute);
+    }
+
+    #[test]
+    fn test_aggregation_equality() {
+        assert_eq!(Aggregation::SumSquared, Aggregation::SumSquared);
+        assert_ne!(Aggregation::SumSquared, Aggregation::SumAbsolute);
+    }
+
+    #[test]
+    fn test_aggregation_clone() {
+        let agg = Aggregation::MaxAbsolute;
+        let cloned = agg;
+        assert_eq!(agg, cloned);
+    }
+
+    #[test]
+    fn test_error_metric_default() {
+        assert_eq!(ErrorMetric::default(), ErrorMetric::Squared);
+    }
+
+    #[test]
+    fn test_error_metric_equality() {
+        assert_eq!(ErrorMetric::Absolute, ErrorMetric::Absolute);
+        assert_ne!(ErrorMetric::Absolute, ErrorMetric::Squared);
+    }
+
+    #[test]
+    fn test_loss_type_default() {
+        let loss = LossType::default();
+        assert_eq!(loss.metric, ErrorMetric::Squared);
+        assert_eq!(loss.aggregation, Aggregation::SumSquared);
+    }
+
+    #[test]
+    fn test_loss_type_region_error() {
+        let loss = LossType::region_error();
+        assert_eq!(loss.metric, ErrorMetric::Squared);
+        assert_eq!(loss.aggregation, Aggregation::SumSquared);
+    }
+
+    #[test]
+    fn test_loss_type_stress() {
+        let loss = LossType::stress();
+        assert_eq!(loss.metric, ErrorMetric::RelativeSquared);
+        assert_eq!(loss.aggregation, Aggregation::SumSquared);
+    }
+
+    #[test]
+    fn test_loss_type_diag_error() {
+        let loss = LossType::diag_error();
+        assert_eq!(loss.metric, ErrorMetric::Relative);
+        assert_eq!(loss.aggregation, Aggregation::SumAbsolute);
+    }
+
+    #[test]
+    fn test_loss_type_minimax() {
+        let loss = LossType::minimax();
+        assert_eq!(loss.metric, ErrorMetric::Absolute);
+        assert_eq!(loss.aggregation, Aggregation::MaxAbsolute);
+    }
+
+    #[test]
+    fn test_loss_type_minimax_relative() {
+        let loss = LossType::minimax_relative();
+        assert_eq!(loss.metric, ErrorMetric::Relative);
+        assert_eq!(loss.aggregation, Aggregation::MaxRelative);
+    }
+
+    #[test]
+    fn test_loss_type_custom() {
+        let loss = LossType::custom(ErrorMetric::Absolute, Aggregation::RootMeanSquared);
+        assert_eq!(loss.metric, ErrorMetric::Absolute);
+        assert_eq!(loss.aggregation, Aggregation::RootMeanSquared);
+    }
+
+    #[test]
+    fn test_loss_type_equality() {
+        let loss1 = LossType::region_error();
+        let loss2 = LossType::region_error();
+        let loss3 = LossType::stress();
+        assert_eq!(loss1, loss2);
+        assert_ne!(loss1, loss3);
+    }
+
+    #[test]
+    fn test_loss_type_clone() {
+        let loss = LossType::minimax();
+        let cloned = loss;
+        assert_eq!(loss, cloned);
+    }
+
+    #[test]
+    fn test_compute_error_absolute() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Absolute,
+            aggregation: Aggregation::SumAbsolute,
+        };
+        assert_eq!(loss.compute_error(5.0, 3.0), 2.0);
+        assert_eq!(loss.compute_error(3.0, 5.0), 2.0);
+        assert_eq!(loss.compute_error(5.0, 5.0), 0.0);
+    }
+
+    #[test]
+    fn test_compute_error_squared() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Squared,
+            aggregation: Aggregation::SumSquared,
+        };
+        assert_eq!(loss.compute_error(5.0, 3.0), 4.0);
+        assert_eq!(loss.compute_error(3.0, 5.0), 4.0);
+        assert_eq!(loss.compute_error(5.0, 5.0), 0.0);
+    }
+
+    #[test]
+    fn test_compute_error_relative() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Relative,
+            aggregation: Aggregation::SumAbsolute,
+        };
+        assert_eq!(loss.compute_error(10.0, 5.0), 1.0);
+        assert_eq!(loss.compute_error(5.0, 10.0), 0.5);
+        assert_eq!(loss.compute_error(5.0, 5.0), 0.0);
+    }
+
+    #[test]
+    fn test_compute_error_relative_zero_target() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Relative,
+            aggregation: Aggregation::SumAbsolute,
+        };
+        // Both zero: no error
+        assert_eq!(loss.compute_error(0.0, 0.0), 0.0);
+        // Target zero, fitted non-zero: error is fitted value
+        assert_eq!(loss.compute_error(5.0, 0.0), 5.0);
+        assert_eq!(loss.compute_error(-3.0, 0.0), 3.0);
+    }
+
+    #[test]
+    fn test_compute_error_relative_squared() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::RelativeSquared,
+            aggregation: Aggregation::SumSquared,
+        };
+        assert_eq!(loss.compute_error(10.0, 5.0), 1.0);
+        assert_eq!(loss.compute_error(5.0, 10.0), 0.25);
+        assert_eq!(loss.compute_error(5.0, 5.0), 0.0);
+    }
+
+    #[test]
+    fn test_compute_error_relative_squared_zero_target() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::RelativeSquared,
+            aggregation: Aggregation::SumSquared,
+        };
+        assert_eq!(loss.compute_error(0.0, 0.0), 0.0);
+        assert_eq!(loss.compute_error(3.0, 0.0), 9.0);
+    }
+
+    #[test]
+    fn test_aggregate_sum_squared() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Absolute,
+            aggregation: Aggregation::SumSquared,
+        };
+        assert_eq!(loss.aggregate(vec![1.0, 2.0, 3.0]), 6.0);
+        assert_eq!(loss.aggregate(vec![]), 0.0);
+    }
+
+    #[test]
+    fn test_aggregate_sum_absolute() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Absolute,
+            aggregation: Aggregation::SumAbsolute,
+        };
+        assert_eq!(loss.aggregate(vec![1.0, 2.0, 3.0]), 6.0);
+        assert_eq!(loss.aggregate(vec![]), 0.0);
+    }
+
+    #[test]
+    fn test_aggregate_max_absolute() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Absolute,
+            aggregation: Aggregation::MaxAbsolute,
+        };
+        assert_eq!(loss.aggregate(vec![1.0, 5.0, 3.0]), 5.0);
+        assert_eq!(loss.aggregate(vec![7.0]), 7.0);
+    }
+
+    #[test]
+    fn test_aggregate_max_relative() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Relative,
+            aggregation: Aggregation::MaxRelative,
+        };
+        assert_eq!(loss.aggregate(vec![0.1, 0.5, 0.3]), 0.5);
+    }
+
+    #[test]
+    fn test_aggregate_root_mean_squared() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Squared,
+            aggregation: Aggregation::RootMeanSquared,
+        };
+        let result = loss.aggregate(vec![4.0, 9.0, 16.0]);
+        assert!((result - 3.1091).abs() < 0.001);
+        assert_eq!(loss.aggregate(vec![16.0]), 4.0);
+    }
+
+    #[test]
+    fn test_loss_function_evaluate() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Squared,
+            aggregation: Aggregation::SumSquared,
+        };
+
+        let mut fitted_areas = HashMap::new();
+        fitted_areas.insert(0b001, 10.0);
+        fitted_areas.insert(0b010, 20.0);
+
+        let mut target_areas = HashMap::new();
+        target_areas.insert(0b001, 12.0);
+        target_areas.insert(0b010, 18.0);
+
+        // (10-12)^2 + (20-18)^2 = 4 + 4 = 8
+        assert_eq!(loss.evaluate(&fitted_areas, &target_areas), 8.0);
+    }
+
+    #[test]
+    fn test_loss_function_evaluate_missing_fitted() {
+        let loss = ConfigurableLoss {
+            metric: ErrorMetric::Squared,
+            aggregation: Aggregation::SumSquared,
+        };
+
+        let fitted_areas = HashMap::new();
+        let mut target_areas = HashMap::new();
+        target_areas.insert(0b001, 5.0);
+
+        // fitted=0, target=5: (0-5)^2 = 25
+        assert_eq!(loss.evaluate(&fitted_areas, &target_areas), 25.0);
+    }
+
+    #[test]
+    fn test_loss_function_name() {
+        let tests = vec![
+            (
+                ErrorMetric::Squared,
+                Aggregation::SumSquared,
+                "region_error",
+            ),
+            (
+                ErrorMetric::RelativeSquared,
+                Aggregation::SumSquared,
+                "stress",
+            ),
+            (
+                ErrorMetric::Relative,
+                Aggregation::SumAbsolute,
+                "diag_error",
+            ),
+            (ErrorMetric::Absolute, Aggregation::MaxAbsolute, "minimax"),
+            (
+                ErrorMetric::Relative,
+                Aggregation::MaxRelative,
+                "minimax_relative",
+            ),
+            (
+                ErrorMetric::Absolute,
+                Aggregation::RootMeanSquared,
+                "custom",
+            ),
+        ];
+
+        for (metric, aggregation, expected_name) in tests {
+            let loss = ConfigurableLoss {
+                metric,
+                aggregation,
+            };
+            assert_eq!(loss.name(), expected_name);
+        }
+    }
+
+    #[test]
+    fn test_loss_type_create() {
+        let loss_type = LossType::region_error();
+        let loss_fn = loss_type.create();
+
+        let mut fitted_areas = HashMap::new();
+        fitted_areas.insert(0b001, 10.0);
+
+        let mut target_areas = HashMap::new();
+        target_areas.insert(0b001, 8.0);
+
+        // (10-8)^2 = 4
+        assert_eq!(loss_fn.evaluate(&fitted_areas, &target_areas), 4.0);
+        assert_eq!(loss_fn.name(), "region_error");
+    }
+
+    #[test]
+    fn test_debug_implementations() {
+        let agg = Aggregation::SumSquared;
+        let debug = format!("{:?}", agg);
+        assert!(debug.contains("SumSquared"));
+
+        let metric = ErrorMetric::Relative;
+        let debug = format!("{:?}", metric);
+        assert!(debug.contains("Relative"));
+
+        let loss = LossType::stress();
+        let debug = format!("{:?}", loss);
+        assert!(debug.contains("RelativeSquared"));
+    }
+}
